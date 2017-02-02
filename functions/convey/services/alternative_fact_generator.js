024374b5d55509fa00911d28dcb718d5a0fbc0ff
ashkan18 <ashkan18@gmail.com>
2017-02-02 07:50:46 -0500

    More facts, better generator

--- functions/convey/services/alternative_fact_generator.js
+++ functions/convey/services/alternative_fact_generator.js
@@ -1,5 +1,11 @@
 module.exports = {
   alternetize: function(actualFact) {
-    return actualFact.replace(' is ', " isn't ").replace(' was ', " wasn't ").replace(' equals ', ' is not equal ').replace(' should ', ' should not ')
+    return actualFact.replace(/ is /ig, " is not ")
+                     .replace(/ was /ig, " was not ")
+                     .replace(/ equals /ig, ' is not equal ')
+                     .replace(/ should /ig, ' should not ')
+                     .replace(/ will /ig, ' will not ')
+                     .replace(/ are /ig, ' are not ')
+                     .replace(/ were /ig, ' were not ')
   }
 }
